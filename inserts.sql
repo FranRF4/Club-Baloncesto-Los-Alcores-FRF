8936a87b5a623031abcb4d1271cff37d600e6699
FranRF4 <fran.rf04@gmail.com>
2024-12-18 11:40:01 +0100

    cambiar BBDD

---
+++ inserts.sql
@@ -0,0 +1,26 @@
+INSERT INTO TEMPORADAS (anio, categoria) VALUES (2024, '2ª Senior Provincial Masculina Sevilla');
+
+
+INSERT INTO EQUIPO (nombre, categoria, escudo, nombre_campo, ubi_campo, color_local, color_visit) VALUES
+('EXCOM CB Puebla', '2ª Senior Provincial Masculina Sevilla', NULL, 'Pabellón Enrique Lora', 'Puebla del Río, Sevilla', 'Desconocido', 'Desconocido'),
+('CB Coria', '2ª Senior Provincial Masculina Sevilla', NULL, 'Pabellón Municipal CB Coria', 'Coria del Río, Sevilla', 'Desconocido', 'Desconocido'),
+('CD Tartessos Lebrija', '2ª Senior Provincial Masculina Sevilla', NULL, 'Pabellón Cubierto 2', 'Lebrija, Sevilla', 'Desconocido', 'Desconocido'),
+('CB Paradas', '2ª Senior Provincial Masculina Sevilla', NULL, 'Pabellón Municipal Francisco Muñoz Moreno', 'Paradas, Sevilla', 'Desconocido', 'Desconocido'),
+('CP Albancor', '2ª Senior Provincial Masculina Sevilla', NULL, 'Pabellón Cubierto Miguel Ángel Gómez Campuzano', 'Ubicación desconocida', 'Desconocido', 'Desconocido'),
+('Juventud Deportiva Bormujos', '2ª Senior Provincial Masculina Sevilla', NULL, 'Pabellón Municipal de Bormujos', 'Bormujos, Sevilla', 'Desconocido', 'Desconocido'),
+('CDB Guillena', '2ª Senior Provincial Masculina Sevilla', NULL, 'Pabellón Municipal de Deportes "La Villa"', 'Guillena, Sevilla', 'Desconocido', 'Desconocido'),
+('CB Los Alcores 2013', '2ª Senior Provincial Masculina Sevilla', NULL, 'Pabellón Santa Lucía', 'Ubicación desconocida', 'Desconocido', 'Desconocido'),
+('Club de Baloncesto Sajuma', '2ª Senior Provincial Masculina Sevilla', NULL, 'Colegio Salesianos Santísima Trinidad (Pabellón)', 'Sevilla', 'Desconocido', 'Desconocido'),
+('Clínica Montaño CDB Herrera', '2ª Senior Provincial Masculina Sevilla', NULL, 'Pabellón Municipal de Deportes', 'Herrera, Sevilla', 'Desconocido', 'Desconocido');
+
+
+-- Suponiendo que la temporada insertada tiene el ID 1
+INSERT INTO JORNADA (dia, mes, temporada) VALUES
+(20, 10, 1), -- Jornada 1
+(27, 10, 1), -- Jornada 2
+(10, 11, 1), -- Jornada 3
+(15, 11, 1), -- Jornada 4
+(24, 11, 1), -- Jornada 5
+(1, 12, 1),  -- Jornada 6
+(15, 12, 1), -- Jornada 7
+(22, 12, 1); -- Jornada 8
